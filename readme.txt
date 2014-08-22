a6220a354d8ef64bcd01e1bf97f71257909352fc
xiaoxu125634 <xiaoxu125634@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-22 01:54:16 +0000

    first submit
    git-svn-id: https://plugins.svn.wordpress.org/useso-take-over-google/trunk@970235 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Useso take over Google ===
+Contributors: xiaoxu125634
+Donate link: http://www.brunoxu.com/
+Tags: Open Sans, Google Fonts, Google Web Fonts, useso, 360前端公共库, Google字体库, Google公共库, Useso公共库, Useso字体库
+Requires at least: 3.0
+Tested up to: 3.9.2
+Stable tag: 1.0
+
+用360前端公共库Useso接管Google字体库和Google公共库，无需设置，插件安装激活后即刻生效。
+
+== Description ==
+
+[插件首页](http://www.brunoxu.com/useso-take-over-google.html) | [插件作者](http://www.brunoxu.com/)
+
+前面做了一个去除Google字体插件：<a href="http://wordpress.org/plugins/remove-google-fonts-references/">Remove Google Fonts References</a>，用来去除所有页面中的Google字体引用，避免网页打开速度被严重拖慢。
+
+如果不去掉Google资源引用，使用360前端公共库会怎么样，因为http://libs.useso.com/提供了国内可访问的替换方案，基于useso就开发出了这个插件：Useso take over Google。
+
+插件会自动把所有页面中出现的对Google字体、Google公共库的引用，换成对useso的引用，保证国内能正常访问资源。
+
+插件无需设置，安装激活后即刻生效。
+
+== Installation ==
+
+1. Upload `useso-take-over-google` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress background.
+
+== Changelog ==
+
+= 1.0 =
+* 2014-08-22
+* First released version.
---
+++ useso-take-over-google.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Useso take over Google
+Plugin URI: http://www.brunoxu.com/useso-take-over-google.html
+Description: 用360前端公共库Useso接管Google字体库和Google公共库，无需设置，插件安装激活后即刻生效。
+Author: Bruno Xu
+Author URI: http://www.brunoxu.com/
+Version: 1.0
+License: GPL
+*/
+
+if (is_admin()) {
+	$action = 'admin_init';
+} else {
+	$action = 'get_header';
+}
+add_action($action, 'useso_take_over_google_obstart');
+function useso_take_over_google_obstart() {
+	ob_start('useso_take_over_google_obend');
+}
+
+function useso_take_over_google_obend($content) {
+	return useso_take_over_google_filter($content);
+}
+
+function useso_take_over_google_filter($content)
+{
+	$regexp = "/<(link|script)([^<>]+)>/i";
+
+	$content = preg_replace_callback(
+		$regexp,
+		"useso_take_over_google_str_handler",
+		$content
+	);
+
+	return $content;
+}
+
+function useso_take_over_google_str_handler($matches)
+{
+	$str = $matches[0];
+
+	$str = str_ireplace('//fonts.googleapis.com/', '//fonts.useso.com/', $str);
+	$str = str_ireplace('//ajax.googleapis.com/', '//ajax.useso.com/', $str);
+
+	return $str;
+}
